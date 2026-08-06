9f6d201c7c6028684e23158a98e0a0c2f739922a
shanlishoes <shanlishoes@gmail.com>
2026-08-07 00:33:06 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='shanli-visit-v2'; const FILES=['/','/cashier.html','/seller.webmanifest','/cashier.webmanifest','/icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));});
